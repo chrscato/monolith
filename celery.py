9c02a4a247db88decb7ba180ace72e0f6f95d4e3
chrscato <62491107+chrscato@users.noreply.github.com>
2025-06-24 15:33:12 -0400

    Update Celery configuration to use cdx_ehr settings

--- celery.py
+++ celery.py
@@ -1,10 +1,10 @@
-# cdx_crm/celery.py (put this next to settings.py)
+# Celery configuration for the cdx_ehr project.
 
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cdx_crm.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cdx_ehr.settings')
 
-app = Celery('cdx_crm')
+app = Celery('cdx_ehr')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
